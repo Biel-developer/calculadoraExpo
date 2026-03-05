2b787677847b233fbf8e18912b521c31bf433e22
Biel-developer <gabriel.naascimento18@gmail.com>
2026-03-04 22:26:06 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,39 @@
+## Descrição
+
+Este aplicativo de calculadora vai ser desenvolvido utilizando React Native com suporte da plataforma Expo.
+
+## Objetivo
+
+O objetivo do projeto é permitir a realização de operações matemáticas básicas de forma rápida, prática e intuitiva em dispositivos móveis com a  proposta de oferecer uma calculadora simples e eficiente.
+
+## Requisitos Funcionais
+
+O aplicativo deve permitir que o usuário insira números de 0 a 9 por meio de botões na tela.
+
+O aplicativo deve permitir a realização das seguintes operações:
+### Adição (+)
+### Subtração (-)
+### Multiplicação (*)
+### Divisão (/)
+
+O aplicativo deve exibir na tela os números digitados e a operação selecionada.
+apresentar o resultado da operação ao pressionar o botão "=".
+deve permitir apagar todos os dados inseridos utilizando o botão "C".
+
+
+## Requisitos Não Funcionais
+
+O aplicativo deve responder imediatamente ao pressionar qualquer botão.
+manter a organização da interface independentemente do tamanho da tela.
+
+## Escopo
+
+Este projeto possui operações matemáticas básicas: adição, subtração, multiplicação e divisão,
+o usuário insere os valores numéricos, seleciona a operação desejada e visualiza o resultado na tela
+não inclui funcionalidades avançadas de operações complexas apenas uma calculadora basica
+
+
+
+# Protótipo 
+<img width="400" height="1200" alt="image" src="https://github.com/user-attachments/assets/21230b1c-4d68-4b93-81d2-89678c24856d" />
+
---
+++ app/_layout.tsx
@@ -0,0 +1,163 @@
+
+import { useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView } from "react-native"
+
+
+export default function Rootlayout() {
+
+  const [valor, setValor] = useState('')
+  const [expressao, setExpressao] = useState('')
+
+  function adicionar(numero: string) {
+    setValor(valor + numero)
+  }
+
+  function limparTela() {
+    setValor('')
+    setExpressao('')
+  }
+
+  function resultadoOperacao() {
+    setValor(eval(valor))
+    setExpressao(valor)
+  }
+
+
+
+  return (
+    <View style={style.container}>
+      <Text style={style.expressao}>{expressao}</Text>
+      <View style={style.scrollView}>
+      <ScrollView>
+        <Text style={style.display}>{valor === '' ? '0' : valor.toLocaleString('pt-BR')}</Text>
+      </ScrollView>
+      </View>
+
+
+      <View style={style.row}>
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('7')}>
+          <Text style={style.textoRow}>7</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('8')}>
+          <Text style={style.textoRow}>8</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('9')}>
+          <Text style={style.textoRow}>9</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('/')}>
+          <Text style={style.textoRow}>/</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={style.row}>
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('4')}>
+          <Text style={style.textoRow}>4</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('5')}>
+          <Text style={style.textoRow}>5</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('6')}>
+          <Text style={style.textoRow}>6</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('*')}>
+          <Text style={style.textoRow}>*</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={style.row}>
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('1')}>
+          <Text style={style.textoRow}>1</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('2')}>
+          <Text style={style.textoRow}>2</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('3')}>
+          <Text style={style.textoRow}>3</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('-')}>
+          <Text style={style.textoRow}>-</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={style.row}>
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('0')}>
+          <Text style={style.textoRow}>0</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={limparTela}>
+          <Text style={style.textoRow}>C</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={resultadoOperacao}>
+          <Text style={style.textoRow}>=</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.botao} onPress={() => adicionar('+')}>
+          <Text style={style.textoRow}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: 15
+  },
+  display: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    color: "#fff",
+    textAlign: "right",
+    fontSize: 85,
+    marginTop: 295
+  },
+  botao: {
+    backgroundColor: '#1e1e1e',
+    width: 85,
+    height: 85,
+    borderRadius: 25,
+    padding: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+    padding: 5,
+    gap: 9
+  },
+  textoRow: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  scrollView:{
+    flex: 1,
+    position: "relative",
+    marginLeft: "auto"
+  },
+  expressao:{
+    position: "absolute",
+    flex: 1,
+    color: "rgba(255, 255, 255, 0.47)",
+    fontSize: 45,
+    marginLeft: 278,
+    marginBottom: 525
+  }
+})
